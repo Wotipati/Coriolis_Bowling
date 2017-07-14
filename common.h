577123e88b0422dcc1c78d506b74baf9319c4206
kayukawa-seita <k940805k.lab@gmail.com>
2017-07-14 16:32:21 +0900

    minor update

--- common.h
+++ common.h
@@ -1,8 +1,9 @@
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
-static const char *SOURCE_DIRECTORY = "/Users/kayukawaao/Desktop/Morishima_Lab/x_code/OpenGL/Coriolis_Bowling/Coriolis_Bowling/";
-static const char *SHADER_DIRECTORY = "/Users/kayukawaao/Desktop/Morishima_Lab/x_code/OpenGL/Coriolis_Bowling/Coriolis_Bowling/shaders/";
-static const char *DATA_DIRECTORY = "/Users/kayukawaao/Desktop/Morishima_Lab/x_code/OpenGL/Coriolis_Bowling/Coriolis_Bowling/data/";
+static const char *SOURCE_DIRECTORY = "./Coriolis_Bowling/";
+static const char *SHADER_DIRECTORY = "./shaders/";
+static const char *DATA_DIRECTORY = "./data/";
+
 
 #endif  // _COMMON_H_
