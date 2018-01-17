7b4894c139ba7035a0809cd880867401f679eea3
Wotipati <k940805k.lab@gmail.com>
2018-01-17 22:46:54 +0900

    delete common.h

--- common.h
+++
@@ -1,7 +0,0 @@
-#ifndef _COMMON_H_
-#define _COMMON_H_
-
-static const char *SHADER_DIRECTORY = "/Users/wotipati/Desktop/Morishima_Lab/x_code/coriolisBowling/shaders/";
-static const char *DATA_DIRECTORY = "/Users/wotipati/Desktop/Morishima_Lab/x_code/coriolisBowling/data/";
-
-#endif  // _COMMON_H_
